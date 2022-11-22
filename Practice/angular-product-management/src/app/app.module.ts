e65c761a171fed4bb946256e8e5669597b7a0b93
Compass <ndphong165@gmail.com>
2022-11-22 21:06:34 +0700

    first commit

---
+++ Practice/angular-product-management/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
+import { ProductDeleteComponent } from './product/product-delete/product-delete.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
